150f31b492e461635dc55dd4e63756c1cccdc6e1
sulaimanbiswas <sulaimanbiswasbd@gmail.com>
2020-12-30 03:10:32 +0600

    --- Auto-Git Commit ---

--- class04/index.php
+++ class04/index.php
@@ -2,20 +2,53 @@
 require '../vendor/autoload.php';
 echo"<pre>";
 
-$student_info = [
-    'name' => 'sulaiman',
-    'phone' => '0147859625',
-    'adress' => [
-        'permanent' => 'meherpur'
-    ],
-    'designation' => 'student',
+// $student_info = [
+//     'name' => 'sulaiman',
+//     'phone' => '0147859625',
+//     'adress' => [
+//         'permanent' => 'meherpur'
+//     ],
+//     'designation' => 'student',
 
-];
+// ];
+// $student_info2 = [
+//     'name' => 'sulaiman',
+//     'phone' => '0147859625',
+//     'adress' => [
+//         'permanent' => 'meherpur'
+//     ],
+//     'designation' => 'student',
 
-dd($student_info);
+// ];
 
 
 
+// var_dump($student_info);
+// var_dump($student_info2);
+
+class UserInfo
+{
+    public $name;
+    public $phone;
+    public $email;
+    public $permanent_adress;
+    public $Present_adress;
+    public $designetaion;
+
+    function __construct($name, $phone, $email) {
+        $this->name = $name;
+        $this->name = $phone;
+        $this->name = $email;
+    }
+}
+
+$user1 = new UserInfo('sulaiman biswas', '01745360890', 'sulaimanbiswasbd@gmail.com');
+
+
+// $user1 -> name ='sulaimanbiswas';
+// $user1 -> phone ='014745630890';
+
+var_dump($user1);
 
 
 
