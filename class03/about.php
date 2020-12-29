d5cf622df0d510ff33548bee6de42d6502c61aaf
sulaimanbiswas <sulaimanbiswasbd@gmail.com>
2020-12-30 00:38:55 +0600

    --- Auto-Git Commit ---

--- class03/about.php
+++ class03/about.php
@@ -15,7 +15,31 @@ echo "<pre>";
 
 // echo $_COOKIE['user'];
 
-$name = "md, sulaiman, biswas";
+$name = "md sulaiman biswas";
+$password = md5($name);
+
+// var_dump(explode(' ',$name));
+echo $name;
+// var_dump(strlen($name));
+// var_dump(md5($name));
+// if($password == md5($name)) {
+//     echo 'true';
+// }else{
+//     echo 'false';
+// }
+
+// var_dump(substr($name,0,11));
+// var_dump(ucwords($name));
+// var_dump(strtoupper($name));
+// var_dump(strtolower($name));
+
+// var_dump(str_shuffle($name));
+// var_dump(str_replace(' ','_',$name));
+
+$arr = array();
+array_push($arr,34);
+
+var_dump($arr);
 
 
 ?>
